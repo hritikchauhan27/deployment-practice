7aff4e5bd266d24cfb6fd4c765cc85c71dec177a
hritik.chauhan <hritik.chauhan@appinventiv.com>
2026-01-21 17:39:05 +0530

    Add health check API

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
   res.send("Hello from DEV");
 });
 
+app.get("/health", (req, res) => {
+    res.json({ status: "OK", env: process.env.ENV || "dev" });
+});  
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
